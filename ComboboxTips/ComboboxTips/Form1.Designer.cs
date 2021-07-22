1b5e9fbcca854ed4a3952962b29c9c559bd341a1
Hasegawa Shingo <hasepon@ae.em-net.ne.jp>
2021-07-22 10:38:22 +0900

    C# ComboBox Tips

---
+++ ComboboxTips/ComboboxTips/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace ComboboxTips
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ComboBox1 = new System.Windows.Forms.ComboBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ComboBox1
+            // 
+            this.ComboBox1.FormattingEnabled = true;
+            this.ComboBox1.Location = new System.Drawing.Point(12, 12);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(272, 26);
+            this.ComboBox1.TabIndex = 0;
+            this.ComboBox1.SelectionChangeCommitted += new System.EventHandler(this.ComboBox1_SelectionChangeCommitted);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(290, 15);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(39, 18);
+            this.Label1.TabIndex = 2;
+            this.Label1.Text = "Value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 52);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.ComboBox1);
+            this.Font = new System.Drawing.Font("メイリオ", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ComboBox1;
+        private System.Windows.Forms.Label Label1;
+    }
+}
+
---
+++ ComboboxTips/ComboboxTips/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace ComboboxTips
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// コンボボックスに設定するソース
+        /// </summary>
+        private readonly Dictionary<string, string> ComboboxSource;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            ComboboxSource = new Dictionary<string, string> { { string.Empty, string.Empty } };
+        }
+
+        /// <summary>
+        /// 画面起動時にコンボボックス設定をします。
+        /// </summary>
+        /// <param name="sender">イベント発行元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 設定ファイルからコンボボックスのデータソースを取得して、ディクショナリに格納します。
+            var comboSources = (NameValueCollection)ConfigurationManager.GetSection("SourceList");
+
+            foreach(string item in comboSources)
+            {
+                ComboboxSource.Add(item, comboSources[item]);
+            }
+
+            // ディクショナリをコンボボックスに設定します。
+            ComboBox1.DataSource = ComboboxSource.ToList();
+            ComboBox1.DisplayMember = "key";
+            ComboBox1.ValueMember = "value";
+        }
+
+        /// <summary>
+        /// 選択時に選択されたkeyに対するvalueを取得してラベルに設定します。
+        /// </summary>
+        /// <param name="sender">イベント発行元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void ComboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            Label1.Text = $"value -> {ComboBox1.SelectedValue}";
+        }
+    }
+}
